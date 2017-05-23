914e675096ec838543b676dc98f76f631070307b
Zakaria KERKEB <zkerkeb@e2r4p22.42.fr>
2017-05-23 23:23:51 +0200

    test on expo online ok, configure the environment on mac

--- front/App.js
+++ front/App.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Button,
+  Alert
+} from 'react-native';
 import { Constants } from 'expo';
 
-const fr = ['bonjour', 'merci', 'comment allez vous ?'];
-const ko = ['안녕하세요', '고맙습니다', '잘 지냈어 요?'];
+const fr = ['bonjour', 'merci', 'comment allez vous ?', 'Viande'];
+const ko = ['안녕하세요', '고맙습니다', '잘 지냈어 요?', '고기'];
 
 export default class App extends Component {
   state = {
     lang: 'fr',
     content: fr,
-    index: 0
+    index: 0,
+    learning: ko,
+    currentWord: null
   };
 
-  change = () => {
-    if (this.state.lang === 'ko') {
-      this.setstate({ lang: 'fr', content: fr });
-    }
-    if (this.state.lang === 'fr') {
-      this.setstate({ lang: 'ko', content: ko });
-    }
+  componentDidMount() {
+    console.log('debut');
+
+    this.getRandomWord();
+    console.log(this.state);
+  }
+
+  // _change() {
+  //   console.log(this.state);
+  //   console.log('CHANGE');
+  //   if (this.state.lang === 'ko') {
+  //     this.setstate({ lang: 'fr', content: fr });
+  //   }
+  //   if (this.state.lang === 'fr') {
+  //     this.setstate({ lang: 'ko', content: ko });
+  //   }
+  // }
+
+  _handleButtonPress = () => {
+    // Alert.alert(
+    //   'Button pressed!',
+    //   'You did it!',
+    // );
+    console.log(this.getRandomInt(1, this.state.learning.length));
+  };
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  switchLang = () => {
+    this.setState({
+      learning: this.state.content,
+      content: this.state.learning
+    });
+    this.setState({ currentWord: this.state.learning[this.state.randomInt] });
+  };
+
+  getRandomWord = () => {
+    this.setState({
+      randomInt: this.getRandomInt(1, this.state.learning.length)
+    });
+    this.setState({ currentWord: this.state.learning[this.state.randomInt] });
   };
 
   render() {
@@ -27,13 +74,19 @@ export default class App extends Component {
         <Text style={styles.paragraph}>
           ALURA
         </Text>
+
         <View style={styles.title}>
-          <Text style={styles.paragraph}>
-            {this.state.lang}
-            {this.state.content}
-            {this.state.index}
-          </Text>
+          <TouchableWithoutFeedback onPress={this._change}>
+            <Text style={styles.paragraph}>
+              {this.state.currentWord}
+            </Text>
+          </TouchableWithoutFeedback>
+          <Button title="show solution" onPress={this._handleButtonPress} />
+          <Button title="Random Word" onPress={this.getRandomWord} />
+          <Button title="Press me" onPress={this.switchLang} />
+
         </View>
+
       </View>
     );
   }
