8b20e9b1ff426d81e004af29a63378953531a071
Zakaria KERKEB <zkerkeb@e2r3p20.42.fr>
2017-05-31 20:54:08 +0200

    Korean added ! 안영하세오

--- app/App.js
+++ app/App.js
@@ -8,17 +8,54 @@ import {
   Alert
 } from 'react-native';
 import { NativeRouter, Route, Link } from 'react-router-native'
-
 import { Constants } from 'expo';
+// import Home from './home';
 
 const fr = ['bonjour', 'merci', 'comment allez vous ?', 'Viande'];
 const ko = ['안녕하세요', '고맙습니다', '잘 지냈어 요?', '고기'];
 
+
 export default class App extends Component {
-// <NativeRouter>
-//
-// </NativeRouter>
+state = {
+  lang: 'fr',
+  content: fr,
+  index: 0,
+  learning: ko,
+  currentWord: null
+};
+render() {
+  return (
+    <View style={styles.title}>
+      <Text style={styles.paragraph}>
+        ALURA
+      </Text>
+
+      <View style={styles.title}>
+        <TouchableWithoutFeedback onPress={this._change}>
+          <View>
+          <Text onPress={this.switchLang} style={styles.paragraph}>
+            // {this.state.currentWord}
+          </Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <Button title="show solution" onPress={this._handleButtonPress} />
+        <Button title="Random Word" onPress={this.getRandomWord} />
+        <Button title="Press me" onPress={this.switchLang} />
+
+      </View>
+      <NativeRouter>
+        <View>
+          <Link to="/">
+            <Text>Home</Text>
+          </Link>
+          <Route exact path="/" component={App}/>
+        </View>
+      </NativeRouter>
+
+    </View>
+  );
 }
+};
 
 
 const styles = StyleSheet.create({
@@ -36,26 +73,19 @@ const styles = StyleSheet.create({
   }
 });
 
-state = {
-  lang: 'fr',
-  content: fr,
-  index: 0,
-  learning: ko,
-  currentWord: null
-};
 
-componentDidMount() {
-  console.log('debut');
-
-  this.getRandomWord();
-  console.log(this.state);
-}
-
-getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+// componentDidMount() {
+//   console.log('debut');
+//
+//   this.getRandomWord();
+//   console.log(this.state);
+// }
+//
+// getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
 // switchLang = () => {
 //   this.setState({
